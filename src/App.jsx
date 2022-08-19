a0e1b9a823bb921d1141067f667cd59c8f32e433
Exequiel Portero <eportero421@gmail.com>
2022-08-19 10:17:33 -0300

    he agregado el saludo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
 
     <>
       <Header />
-      <ItemListContainer test="Compras y ventas minoristas" />
+      <ItemListContainer test="Compras y ventas minoristas" saludo="Bienvenidos a mi sitio" />
       <Footer />
     </>
   );
--- src/components/main/ItemListContainer.jsx
+++ src/components/main/ItemListContainer.jsx
@@ -8,7 +8,7 @@ import "animate.css";
 const ItemListContainer = (props) => {
   //props es un objeto, para acceder al valor del main es props.test mediante llaves para decirle que es js
 
-  const { test } = props; //creamos una constante para llamar a cada una de las propiedades
+  const { test,saludo } = props; //creamos una constante para llamar a cada una de las propiedades
 
   return (
     <main className={estilosM.MainEstilo}>
@@ -18,6 +18,8 @@ const ItemListContainer = (props) => {
         <Layout>
           <h2 style={{ fontWeight: "bolder" }} className="animate__animated animate__bounceIn">{test}</h2>
         </Layout>
+
+        <h2 style={{textAlign='center'}}>{saludo}</h2>
       </section>
     </main>
   );
