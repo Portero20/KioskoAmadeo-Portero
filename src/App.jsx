f211ed2ccc073ebff2f81952eb87035519fba18b
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2022-09-21 12:13:27 -0300

    borrando comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,6 @@ import CartProvider from "./components/context/CartContext";
 // import { db } from "../src/components/firebase/firebaseConfig";
 // import { products } from "./components/mock/productos";
 
-
-//podemos hacer un export de una constante, tiene un import diferente
-
-//export const App = () => "Hola mundo"  // los componentes inician con la letra inicial en Mayuscula
-
-//a Main le pasamos una clave-valor, lo que pasamos va a estar en llaves pero un string va en comillas, componente padre le pasa una propiedad a componente hijo
-
-
 // const ref = collection(db,"productos")
 
 // products.map((producto) => addDoc(ref,producto));
@@ -25,7 +17,7 @@ import CartProvider from "./components/context/CartContext";
 const App = () => {
 
   return (
-    //JSX solo debe retornar un solo elemento, no es lo ideal tener muchas etiquetas dentro porque con react creamos componentes
+    
 
     <CartProvider>
 
@@ -41,7 +33,6 @@ const App = () => {
   );
 };
 
-//Fragments: podemos quitar las etiquetas y dejarlo vacio, funciona igual pero con la diferencia que es invisible para el DOM
-//Fragment sirve para el caso de que no quieras crear una etiqueta, tmb sirve para cuando querramos retornar mas de un elemento
 
-export default App; //exportamos el componente para poder consumirlo en otro lado, solo debe haber uno por archivo
+
+export default App; 
