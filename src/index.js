07c53cb7232000a51c5bae6fb4bfee537df4a7a6
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2022-09-24 17:01:45 -0300

    eliminando comentarios

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-//Esto solo pasara en el index (un lugar solo de nuestro proyecto)
-//Webpack es un empaquetador de modulos (create-react-app funciona con webpack por defecto)
+import React from 'react';  
+import ReactDOM from 'react-dom/client';  
 
-import React from 'react';  //importamos la libreria de React (es lo primero que se hace)
-import ReactDOM from 'react-dom/client';  //importamos la libreria de react-dom (nos ayuda a renderizar nuestra app en el DOM)
+import App from './App' 
 
-import App from './App'  //importamos el archivo App.jsx -> export default
-//import {App} from './App'  //importamos la constante entre llaves para que funcione -> export nombrado
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root')); //le indicamos un punto de entrada (createRoot es para poder mostrarlo en el DOM)
-
-root.render(  //empezamos a renderizar nuestra app, aqui voy a mostrar toda mi app (podemos renderizar un componente)
+root.render(  
 
     <React.StrictMode>
 
@@ -21,11 +13,5 @@ root.render(  //empezamos a renderizar nuestra app, aqui voy a mostrar toda mi a
 
     </React.StrictMode>
 
-); 
-
-
-//<App/> es la forma de ejecutar la funcion en react, asi llamamos a las funciones
-//App() ponerlo de esta forma no es la que recomienda react porque va a tirar un warning porque no es react
-
-//React.StrictMode hace un doble chequeo
+);
 
